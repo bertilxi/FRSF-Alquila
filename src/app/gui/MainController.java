0c979d5ecb43fc996c112a9e4857144739441a08
emigioria <emigioria@hotmail.com>
2016-09-18 02:02:51 -0300

    Corregido controlador de ejemplo

--- src/app/gui/MainController.java
+++ src/app/gui/MainController.java
@@ -1,19 +1,11 @@
 package app.gui;
 
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
-import javafx.scene.control.TextField;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-import java.util.regex.Pattern;
+public class MainController {
 
-public class MainController implements Initializable {
-
-	public void initialize(URL fxmlFileLocation, ResourceBundle resources) {
-
+	@FXML
+	private void initialize() {
+		//Inicializar
 	}
-
 }
