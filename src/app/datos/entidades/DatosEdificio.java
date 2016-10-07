33dbdea1ad448cf2c0c271a2b99d57bed51b5d54
Andres <andres.rico94@gmail.com>
2016-10-07 19:01:35 -0300

    quitados comentarios sin uso

--- src/app/datos/entidades/DatosEdificio.java
+++ src/app/datos/entidades/DatosEdificio.java
@@ -4,22 +4,20 @@ package app.datos.entidades;
  * Created by fer on 07/10/16.
  */
 public class DatosEdificio {
-    Integer id;
-    Boolean propiedadHorizontal;
-    Double superficie; // en metros cuadrados
-    Integer antiguedad; // en años
-    // dormitorios, baños, garaje/cochera, patio, piscina, agua corriente, cloacas,
-    // gas natural, agua caliente, teléfono, lavadero, pavimento;
-    Integer dormitorios;
-    Integer baños;
-    Integer garaje;
-    Integer patio;
-    Integer piscina;
-    Boolean aguaCorriente;
-    Boolean cloacas;
-    Boolean gasNatural;
-    Boolean aguaCaliente;
-    String telefono;
-    Boolean lavadero;
-    Boolean pavimento;
+	Integer id;
+	Boolean propiedadHorizontal;
+	Double superficie; // en metros cuadrados
+	Integer antiguedad; // en años
+	Integer dormitorios;
+	Integer baños;
+	Integer garaje;
+	Integer patio;
+	Integer piscina;
+	Boolean aguaCorriente;
+	Boolean cloacas;
+	Boolean gasNatural;
+	Boolean aguaCaliente;
+	String telefono;
+	Boolean lavadero;
+	Boolean pavimento;
 }
