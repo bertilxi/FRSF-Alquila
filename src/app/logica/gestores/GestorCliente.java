d0869fed7542066f4398a8df6031c33be8d52bb8
EstebanRebechi <estebanrebechi_5@hotmail.com>
2016-10-20 17:40:06 -0300

    logica cliente

--- src/app/logica/gestores/GestorCliente.java
+++ src/app/logica/gestores/GestorCliente.java
@@ -1,62 +1,68 @@
 package app.logica.gestores;
 
+import java.util.ArrayList;
 import java.util.regex.Pattern;
 
 import org.springframework.stereotype.Service;
 
 import app.datos.entidades.Cliente;
 import app.datos.servicios.ClienteService;
-import app.excepciones.ObjNotFoundException;
 import app.excepciones.PersistenciaException;
 import app.logica.resultados.ResultadoCrearCliente;
+import app.logica.resultados.ResultadoCrearCliente.ErrorResultadoCrearCliente;
 
 @Service
 public class GestorCliente {
-	
+
 	private ClienteService persistidorCliente;
 
 	public ResultadoCrearCliente crearCliente(Cliente cliente) throws PersistenciaException {
-		ResultadoCrearCliente resultado = new ResultadoCrearCliente();
-		
+		ArrayList<ErrorResultadoCrearCliente> errores = new ArrayList<ErrorResultadoCrearCliente>();
+
+
 		Pattern pat = Pattern.compile("[a-zA-Z\\ ÁÉÍÓÚÜÑáéíóúüñ]{1,100}");
 		if (!pat.matcher(cliente.getNombre()).matches()) {
-			//informar error en clase resultado
+			errores.add(ErrorResultadoCrearCliente.Formato_Nombre_Incorrecto);
 		}
-		
+
 		if (!pat.matcher(cliente.getApellido()).matches()) {
-			//informar error en clase resultado
+			errores.add(ErrorResultadoCrearCliente.Formato_Apellido_Incorrecto);
 		}
-		
+
 		pat = Pattern.compile("[0-9\\-]{0,20}");
 		if (!pat.matcher(cliente.getTelefono()).matches()) {
-			//informar error en clase resultado
+			errores.add(ErrorResultadoCrearCliente.Formato_Telefono_Incorrecto);
 		}
 
 		switch (cliente.getTipoDocumento().getTipo()) {
-		case DNI: 
+		case DNI:
 			pat = Pattern.compile("[0-9]{7,8}");
 			break;
-		case LC: 
+		case LC:
 			pat = Pattern.compile("[0-9\\-]{0,20}");
 			break;
-		case LE: 
+		case LE:
 			pat = Pattern.compile("[0-9\\-]{0,20}");
 			break;
-		case Pasaporte: 
+		case Pasaporte:
 			pat = Pattern.compile("[0-9\\-]{0,20}");
 			break;
-		case CedulaExtranjera: 
+		case CedulaExtranjera:
 			pat = Pattern.compile("[0-9\\-]{0,20}");
 			break;
 		}
 		if (!pat.matcher(cliente.getNumeroDocumento()).matches()) {
-			//informar error en clase resultado
+			errores.add(ErrorResultadoCrearCliente.Formato_Documento_Incorrecto);
 		}
-		
+
 		if(null!=persistidorCliente.obtenerCliente(cliente.getTipoDocumento(), cliente.getNumeroDocumento())) {
-			//informar que ya existe un cliente con este tipo de documento y documento
+			errores.add(ErrorResultadoCrearCliente.Ya_Existe_Cliente);
 		}
-		
-		return resultado;
+
+		if(errores.isEmpty()) {
+			persistidorCliente.guardarCliente(cliente);
+		}
+
+		return new ResultadoCrearCliente(errores.toArray(new ErrorResultadoCrearCliente[0]));
 	}
 }
