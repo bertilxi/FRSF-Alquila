5ab75e3c0e713ddfac974e63f68e45768e76cc4b
Fernando <bertilxi@gmail.com>
2016-10-20 19:39:46 -0300

    controlador propietario

--- src/app/ui/controladores/AltaVendedorController.java
+++ src/app/ui/controladores/AltaVendedorController.java
@@ -1,4 +1,24 @@
 package app.ui.controladores;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+
 public class AltaVendedorController extends BaseController {
+
+
+    @FXML
+    TextField textFieldNombre;
+
+    @FXML
+    TextField textFieldApellido;
+    @FXML
+    TextField textFieldNumeroDocumento;
+    @FXML
+    TextField textFieldContraseña;
+    @FXML
+    TextField textFieldRepiteContraseña;
+    @FXML
+    ComboBox comboBoxTipoDocumento;
 }
--- src/app/ui/controladores/WindowTitleController.java
+++ src/app/ui/controladores/WindowTitleController.java
@@ -10,9 +10,6 @@ import javafx.stage.Stage;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-/**
- * Created by fer on 19/10/16.
- */
 public class WindowTitleController implements Initializable {
 
 
