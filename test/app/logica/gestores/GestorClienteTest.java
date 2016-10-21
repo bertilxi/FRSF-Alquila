08e8b1e760214cf8a748a137dc4d4fa0485af5dd
Fernando <bertilxi@gmail.com>
2016-10-21 16:19:50 -0300

    Mokitooooooo

--- test/app/logica/gestores/GestorClienteTest.java
+++ test/app/logica/gestores/GestorClienteTest.java
@@ -1,25 +1,43 @@
-package test.app.logica.gestores;
+package app.logica.gestores;
 
 import app.datos.entidades.Cliente;
 import app.logica.gestores.GestorCliente;
 import app.logica.resultados.ResultadoCrearCliente;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 
 import static app.logica.resultados.ResultadoCrearCliente.ErrorResultadoCrearCliente.Formato_Nombre_Incorrecto;
 
 
 public class GestorClienteTest {
+
+    @Mock
+    private GestorCliente gestorClienteMock;
+
+    @Mock
+    private Cliente clienteMock;
+
+    @Mock ResultadoCrearCliente resultadoCrearClienteMock;
+
+    @BeforeClass
+    public static void setUp(){
+
+    }
+
     @Test
     public void crearCliente() throws Exception {
 
-        GestorCliente gestorCliente = new GestorCliente();
+        // lo que deberia ser
+        Mockito.when(gestorClienteMock.crearCliente(clienteMock)).thenReturn(resultadoCrearClienteMock);
 
-        Cliente cliente = new Cliente(0, "", "", "", "");
-        ResultadoCrearCliente resultadoCrearClienteOK = new ResultadoCrearCliente(Formato_Nombre_Incorrecto);
+        // lo que devuelve el metodo
+        ResultadoCrearCliente resultadoCrearCliente = gestorClienteMock.crearCliente(clienteMock)
 
+        Assert.assertEquals(resultadoCrearCliente,resultadoCrearClienteMock);
 
-        ResultadoCrearCliente resultadoCrearCliente = gestorCliente.crearCliente(cliente);
 
     }
 
