b6510f269033cd6585f62810b61e97b1256e4354
emigioria <emigioria@hotmail.com>
2016-10-21 15:45:00 -0300

    Movido test

--- src/app/datos/entidades/InmuebleBuscado.java
+++ src/app/datos/entidades/InmuebleBuscado.java
@@ -14,76 +14,76 @@ import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="inmueble_buscado")
+@Table(name = "inmueble_buscado")
 public class InmuebleBuscado {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	
-	@Column(name="precio_max")
-    private Double precioMax;
-	
-	@Column(name="superficie_min")
-    private Double superficieMin; // en metros cuadrados
-    
-	@Column(name="antiguedad_max")
+
+	@Column(name = "precio_max")
+	private Double precioMax;
+
+	@Column(name = "superficie_min")
+	private Double superficieMin; // en metros cuadrados
+
+	@Column(name = "antiguedad_max")
 	private Integer antiguedadMax; // en años
- 
-	@Column(name="dormitorios_min")
+
+	@Column(name = "dormitorios_min")
 	private Integer dormitoriosMin;
-	
-	@Column(name="baños_min")
-    private Integer bañosMin;
-	
-	@Column(name="garaje")
-    private Boolean garaje;
-	
-	@Column(name="patio")
-    private Boolean patio;
-	
-	@Column(name="piscina")
-    private Boolean piscina;
-	
-	@Column(name="propiedad_horizonal")
-    private Boolean propiedadHorizontal;
-	
-	@Column(name="agua_corriente")
-    private Boolean aguaCorriente;
-	
-	@Column(name="cloacas")
-    private Boolean cloacas;
-	
-	@Column(name="gas_natural")
-    private Boolean gasNatural;
-	
-	@Column(name="agua_caliente")
-    private Boolean aguaCaliente;
 
-	@Column(name="lavadero")
-    private Boolean lavadero;
+	@Column(name = "baños_min")
+	private Integer bañosMin;
 
-	@Column(name="pavimento")
-    private Boolean pavimento;
+	@Column(name = "garaje")
+	private Boolean garaje;
 
-    //Relaciones
+	@Column(name = "patio")
+	private Boolean patio;
+
+	@Column(name = "piscina")
+	private Boolean piscina;
+
+	@Column(name = "propiedad_horizonal")
+	private Boolean propiedadHorizontal;
+
+	@Column(name = "agua_corriente")
+	private Boolean aguaCorriente;
+
+	@Column(name = "cloacas")
+	private Boolean cloacas;
+
+	@Column(name = "gas_natural")
+	private Boolean gasNatural;
+
+	@Column(name = "agua_caliente")
+	private Boolean aguaCaliente;
+
+	@Column(name = "lavadero")
+	private Boolean lavadero;
+
+	@Column(name = "pavimento")
+	private Boolean pavimento;
+
+	//Relaciones
 	@ManyToMany(fetch = FetchType.LAZY)
-	@JoinTable(name = "inmueble_buscado_localidad",joinColumns=@JoinColumn(name="idlocalidad"),inverseJoinColumns=@JoinColumn(name="idinmueblebuscado"))
-    private ArrayList<Localidad> localidades;
-	
+	@JoinTable(name = "inmueble_buscado_localidad", joinColumns = @JoinColumn(name = "idlocalidad"), inverseJoinColumns = @JoinColumn(name = "idinmueblebuscado"))
+	private ArrayList<Localidad> localidades;
+
 	@ManyToMany(fetch = FetchType.LAZY)
-	@JoinTable(name = "inmueble_buscado_barrio",joinColumns=@JoinColumn(name="idbarrio"),inverseJoinColumns=@JoinColumn(name="idinmueblebuscado"))
-    private ArrayList<Barrio> barrios; //Relacion muchos a muchos 
-	
+	@JoinTable(name = "inmueble_buscado_barrio", joinColumns = @JoinColumn(name = "idbarrio"), inverseJoinColumns = @JoinColumn(name = "idinmueblebuscado"))
+	private ArrayList<Barrio> barrios; //Relacion muchos a muchos 
+
 	@ManyToMany(fetch = FetchType.LAZY)
-	@JoinTable(name = "inmueble_buscado_tipo_inmueble",joinColumns=@JoinColumn(name="idtipoinmueble"),inverseJoinColumns=@JoinColumn(name="idinmueblebuscado"))
-    private ArrayList<TipoInmueble> tiposInmueblesBuscados; //Relacion muchos a muchos
-	
+	@JoinTable(name = "inmueble_buscado_tipo_inmueble", joinColumns = @JoinColumn(name = "idtipoinmueble"), inverseJoinColumns = @JoinColumn(name = "idinmueblebuscado"))
+	private ArrayList<TipoInmueble> tiposInmueblesBuscados; //Relacion muchos a muchos
+
 	public InmuebleBuscado() {
 		super();
-		this.localidades = new ArrayList<Localidad>();
-		this.barrios = new ArrayList<Barrio>();
-		this.tiposInmueblesBuscados = new ArrayList<TipoInmueble>();
+		this.localidades = new ArrayList<>();
+		this.barrios = new ArrayList<>();
+		this.tiposInmueblesBuscados = new ArrayList<>();
 	}
 
 	public InmuebleBuscado(Integer id, Double precioMax, Double superficieMin, Integer antiguedadMax,
@@ -293,108 +293,168 @@ public class InmuebleBuscado {
 
 	@Override
 	public boolean equals(Object obj) {
-		if (this == obj)
+		if(this == obj){
 			return true;
-		if (obj == null)
+		}
+		if(obj == null){
 			return false;
-		if (getClass() != obj.getClass())
+		}
+		if(getClass() != obj.getClass()){
 			return false;
+		}
 		InmuebleBuscado other = (InmuebleBuscado) obj;
-		if (aguaCaliente == null) {
-			if (other.aguaCaliente != null)
+		if(aguaCaliente == null){
+			if(other.aguaCaliente != null){
 				return false;
-		} else if (!aguaCaliente.equals(other.aguaCaliente))
+			}
+		}
+		else if(!aguaCaliente.equals(other.aguaCaliente)){
 			return false;
-		if (aguaCorriente == null) {
-			if (other.aguaCorriente != null)
+		}
+		if(aguaCorriente == null){
+			if(other.aguaCorriente != null){
 				return false;
-		} else if (!aguaCorriente.equals(other.aguaCorriente))
+			}
+		}
+		else if(!aguaCorriente.equals(other.aguaCorriente)){
 			return false;
-		if (antiguedadMax == null) {
-			if (other.antiguedadMax != null)
+		}
+		if(antiguedadMax == null){
+			if(other.antiguedadMax != null){
 				return false;
-		} else if (!antiguedadMax.equals(other.antiguedadMax))
+			}
+		}
+		else if(!antiguedadMax.equals(other.antiguedadMax)){
 			return false;
-		if (barrios == null) {
-			if (other.barrios != null)
+		}
+		if(barrios == null){
+			if(other.barrios != null){
 				return false;
-		} else if (!barrios.equals(other.barrios))
+			}
+		}
+		else if(!barrios.equals(other.barrios)){
 			return false;
-		if (bañosMin == null) {
-			if (other.bañosMin != null)
+		}
+		if(bañosMin == null){
+			if(other.bañosMin != null){
 				return false;
-		} else if (!bañosMin.equals(other.bañosMin))
+			}
+		}
+		else if(!bañosMin.equals(other.bañosMin)){
 			return false;
-		if (cloacas == null) {
-			if (other.cloacas != null)
+		}
+		if(cloacas == null){
+			if(other.cloacas != null){
 				return false;
-		} else if (!cloacas.equals(other.cloacas))
+			}
+		}
+		else if(!cloacas.equals(other.cloacas)){
 			return false;
-		if (dormitoriosMin == null) {
-			if (other.dormitoriosMin != null)
+		}
+		if(dormitoriosMin == null){
+			if(other.dormitoriosMin != null){
 				return false;
-		} else if (!dormitoriosMin.equals(other.dormitoriosMin))
+			}
+		}
+		else if(!dormitoriosMin.equals(other.dormitoriosMin)){
 			return false;
-		if (garaje == null) {
-			if (other.garaje != null)
+		}
+		if(garaje == null){
+			if(other.garaje != null){
 				return false;
-		} else if (!garaje.equals(other.garaje))
+			}
+		}
+		else if(!garaje.equals(other.garaje)){
 			return false;
-		if (gasNatural == null) {
-			if (other.gasNatural != null)
+		}
+		if(gasNatural == null){
+			if(other.gasNatural != null){
 				return false;
-		} else if (!gasNatural.equals(other.gasNatural))
+			}
+		}
+		else if(!gasNatural.equals(other.gasNatural)){
 			return false;
-		if (id == null) {
-			if (other.id != null)
+		}
+		if(id == null){
+			if(other.id != null){
 				return false;
-		} else if (!id.equals(other.id))
+			}
+		}
+		else if(!id.equals(other.id)){
 			return false;
-		if (lavadero == null) {
-			if (other.lavadero != null)
+		}
+		if(lavadero == null){
+			if(other.lavadero != null){
 				return false;
-		} else if (!lavadero.equals(other.lavadero))
+			}
+		}
+		else if(!lavadero.equals(other.lavadero)){
 			return false;
-		if (localidad == null) {
-			if (other.localidades != null)
+		}
+		if(localidad == null){
+			if(other.localidades != null){
 				return false;
-		} else if (!localidades.equals(other.localidades))
+			}
+		}
+		else if(!localidades.equals(other.localidades)){
 			return false;
-		if (patio == null) {
-			if (other.patio != null)
+		}
+		if(patio == null){
+			if(other.patio != null){
 				return false;
-		} else if (!patio.equals(other.patio))
+			}
+		}
+		else if(!patio.equals(other.patio)){
 			return false;
-		if (pavimento == null) {
-			if (other.pavimento != null)
+		}
+		if(pavimento == null){
+			if(other.pavimento != null){
 				return false;
-		} else if (!pavimento.equals(other.pavimento))
+			}
+		}
+		else if(!pavimento.equals(other.pavimento)){
 			return false;
-		if (piscina == null) {
-			if (other.piscina != null)
+		}
+		if(piscina == null){
+			if(other.piscina != null){
 				return false;
-		} else if (!piscina.equals(other.piscina))
+			}
+		}
+		else if(!piscina.equals(other.piscina)){
 			return false;
-		if (precioMax == null) {
-			if (other.precioMax != null)
+		}
+		if(precioMax == null){
+			if(other.precioMax != null){
 				return false;
-		} else if (!precioMax.equals(other.precioMax))
+			}
+		}
+		else if(!precioMax.equals(other.precioMax)){
 			return false;
-		if (propiedadHorizontal == null) {
-			if (other.propiedadHorizontal != null)
+		}
+		if(propiedadHorizontal == null){
+			if(other.propiedadHorizontal != null){
 				return false;
-		} else if (!propiedadHorizontal.equals(other.propiedadHorizontal))
+			}
+		}
+		else if(!propiedadHorizontal.equals(other.propiedadHorizontal)){
 			return false;
-		if (superficieMin == null) {
-			if (other.superficieMin != null)
+		}
+		if(superficieMin == null){
+			if(other.superficieMin != null){
 				return false;
-		} else if (!superficieMin.equals(other.superficieMin))
+			}
+		}
+		else if(!superficieMin.equals(other.superficieMin)){
 			return false;
-		if (tiposInmueblesBuscados == null) {
-			if (other.tiposInmueblesBuscados != null)
+		}
+		if(tiposInmueblesBuscados == null){
+			if(other.tiposInmueblesBuscados != null){
 				return false;
-		} else if (!tiposInmueblesBuscados.equals(other.tiposInmueblesBuscados))
+			}
+		}
+		else if(!tiposInmueblesBuscados.equals(other.tiposInmueblesBuscados)){
 			return false;
+		}
 		return true;
 	}
 }
