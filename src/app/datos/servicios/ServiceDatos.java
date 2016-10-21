96b5f7fd11dd5bc2d0ddaab0401f65182d5b9bd8
EstebanRebechi <estebanrebechi_5@hotmail.com>
2016-10-21 17:18:56 -0300

    service datos

--- src/app/datos/servicios/ServiceDatos.java
+++ src/app/datos/servicios/ServiceDatos.java
@@ -4,9 +4,13 @@ import java.util.ArrayList;
 
 import app.datos.entidades.Localidad;
 import app.datos.entidades.Provincia;
+import app.datos.entidades.TipoDocumento;
+import app.datos.entidades.TipoInmueble;
 
 public interface ServiceDatos {
 
 	public ArrayList<Localidad> obtenerLocalidadesDe(Provincia provincia);
 	public ArrayList<Provincia> obtenerProvincias();
+	public ArrayList<TipoDocumento> obtenerTiposDeDocumento();
+	public ArrayList<TipoInmueble> obtenerTiposDeInmueble();
 }
