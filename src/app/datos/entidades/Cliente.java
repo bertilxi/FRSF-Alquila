c561c3e57029aef385fa5542564d339fb2ba787f
emigioria <emigioria@hotmail.com>
2016-10-21 17:20:29 -0300

    Agregada entidad estado y continuadas las entidades Cliente e Inmueble

--- src/app/datos/entidades/Cliente.java
+++ src/app/datos/entidades/Cliente.java
@@ -1,154 +1,180 @@
 package app.datos.entidades;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
+@Entity
+@Table(name = "cliente", uniqueConstraints = @UniqueConstraint(name = "cliente_numerodocumento_idtipo_uk", columnNames = { "numerodocumento", "idtipo" }))
 public class Cliente {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
-    private Integer id; //ID
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Integer id; //ID
 
-    @Column(name = "nombre", length = 100)
-    private String nombre;
+	@Column(name = "nombre", length = 100)
+	private String nombre;
 
-    @Column(name = "apellido", length = 100)
-    private String apellido;
+	@Column(name = "apellido", length = 100)
+	private String apellido;
 
-    @Column(name = "numeroDocumento", length = 20)
-    private String numeroDocumento;
+	@Column(name = "numerodocumento", length = 20)
+	private String numeroDocumento;
 
-    @Column(name = "telefono", length = 20)
-    private String telefono;
+	@Column(name = "telefono", length = 20)
+	private String telefono;
 
-    //Relaciones
+	//Relaciones
 
-    @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "idtipodocumento", referencedColumnName = "id", foreignKey = @ForeignKey(name = "cliente_idtipodocumento_fk"))
-    private TipoDocumento tipoDocumento;
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "idtipodocumento", referencedColumnName = "id", foreignKey = @ForeignKey(name = "cliente_idtipodocumento_fk"))
+	private TipoDocumento tipoDocumento;
 
-    //Opcionales
-    // private InmuebleBuscado buscado;
-    // private ArrayList<Venta> compras;
-    // private ArrayList<Reserva> reservas;
-    // private ArrayList<Catalogo> catalogos;
+	//Opcionales
+	// private InmuebleBuscado buscado;
+	// private ArrayList<Venta> compras;
+	// private ArrayList<Reserva> reservas;
+	// private ArrayList<Catalogo> catalogos;
 
-    public Cliente() {
-        super();
-    }
+	public Cliente() {
+		super();
+	}
 
-    public Cliente(Integer id, String nombre, String apellido, String numeroDocumento, String telefono) {
-        this();
-        this.id = id;
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.numeroDocumento = numeroDocumento;
-        this.telefono = telefono;
-    }
+	public Cliente(Integer id, String nombre, String apellido, String numeroDocumento, String telefono) {
+		this();
+		this.id = id;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.numeroDocumento = numeroDocumento;
+		this.telefono = telefono;
+	}
 
-    public Integer getId() {
-        return id;
-    }
+	public Integer getId() {
+		return id;
+	}
 
-    public String getNombre() {
-        return nombre;
-    }
+	public String getNombre() {
+		return nombre;
+	}
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
 
-    public String getApellido() {
-        return apellido;
-    }
+	public String getApellido() {
+		return apellido;
+	}
 
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
 
-    public String getNumeroDocumento() {
-        return numeroDocumento;
-    }
+	public String getNumeroDocumento() {
+		return numeroDocumento;
+	}
 
-    public void setNumeroDocumento(String numeroDocumento) {
-        this.numeroDocumento = numeroDocumento;
-    }
+	public void setNumeroDocumento(String numeroDocumento) {
+		this.numeroDocumento = numeroDocumento;
+	}
 
-    public String getTelefono() {
-        return telefono;
-    }
+	public String getTelefono() {
+		return telefono;
+	}
 
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
 
-    public TipoDocumento getTipoDocumento() {
-        return tipoDocumento;
-    }
+	public TipoDocumento getTipoDocumento() {
+		return tipoDocumento;
+	}
 
-    public void setTipoDocumento(TipoDocumento tipoDocumento) {
-        this.tipoDocumento = tipoDocumento;
-    }
+	public void setTipoDocumento(TipoDocumento tipoDocumento) {
+		this.tipoDocumento = tipoDocumento;
+	}
 
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((apellido == null) ? 0 : apellido.hashCode());
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
-        result = prime * result + ((numeroDocumento == null) ? 0 : numeroDocumento.hashCode());
-        result = prime * result + ((telefono == null) ? 0 : telefono.hashCode());
-        return result;
-    }
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((apellido == null) ? 0 : apellido.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+		result = prime * result + ((numeroDocumento == null) ? 0 : numeroDocumento.hashCode());
+		result = prime * result + ((telefono == null) ? 0 : telefono.hashCode());
+		result = prime * result + ((tipoDocumento == null) ? 0 : tipoDocumento.hashCode());
+		return result;
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        Cliente other = (Cliente) obj;
-        if (apellido == null) {
-            if (other.apellido != null) {
-                return false;
-            }
-        } else if (!apellido.equals(other.apellido)) {
-            return false;
-        }
-        if (id == null) {
-            if (other.id != null) {
-                return false;
-            }
-        } else if (!id.equals(other.id)) {
-            return false;
-        }
-        if (nombre == null) {
-            if (other.nombre != null) {
-                return false;
-            }
-        } else if (!nombre.equals(other.nombre)) {
-            return false;
-        }
-        if (numeroDocumento == null) {
-            if (other.numeroDocumento != null) {
-                return false;
-            }
-        } else if (!numeroDocumento.equals(other.numeroDocumento)) {
-            return false;
-        }
-        if (telefono == null) {
-            if (other.telefono != null) {
-                return false;
-            }
-        } else if (!telefono.equals(other.telefono)) {
-            return false;
-        }
-        return true;
-    }
+	@Override
+	public boolean equals(Object obj) {
+		if(this == obj){
+			return true;
+		}
+		if(obj == null){
+			return false;
+		}
+		if(getClass() != obj.getClass()){
+			return false;
+		}
+		Cliente other = (Cliente) obj;
+		if(apellido == null){
+			if(other.apellido != null){
+				return false;
+			}
+		}
+		else if(!apellido.equals(other.apellido)){
+			return false;
+		}
+		if(id == null){
+			if(other.id != null){
+				return false;
+			}
+		}
+		else if(!id.equals(other.id)){
+			return false;
+		}
+		if(nombre == null){
+			if(other.nombre != null){
+				return false;
+			}
+		}
+		else if(!nombre.equals(other.nombre)){
+			return false;
+		}
+		if(numeroDocumento == null){
+			if(other.numeroDocumento != null){
+				return false;
+			}
+		}
+		else if(!numeroDocumento.equals(other.numeroDocumento)){
+			return false;
+		}
+		if(telefono == null){
+			if(other.telefono != null){
+				return false;
+			}
+		}
+		else if(!telefono.equals(other.telefono)){
+			return false;
+		}
+		if(tipoDocumento == null){
+			if(other.tipoDocumento != null){
+				return false;
+			}
+		}
+		else if(!tipoDocumento.equals(other.tipoDocumento)){
+			return false;
+		}
+		return true;
+	}
 }
---
+++ src/app/datos/entidades/Estado.java
@@ -0,0 +1,83 @@
+package app.datos.entidades;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import app.datos.clases.EstadoStr;
+
+@Entity
+@Table(name = "estado")
+public class Estado {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id; //ID
+
+	@Column(name = "estado")
+	@Enumerated(EnumType.STRING)
+	private EstadoStr estado;
+
+	public Estado() {
+		super();
+	}
+
+	public Estado(Integer id, EstadoStr estado) {
+		super();
+		this.id = id;
+		this.estado = estado;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public EstadoStr getEstado() {
+		return estado;
+	}
+
+	public Estado setEstado(EstadoStr estado) {
+		this.estado = estado;
+		return this;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((estado == null) ? 0 : estado.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if(this == obj){
+			return true;
+		}
+		if(obj == null){
+			return false;
+		}
+		if(getClass() != obj.getClass()){
+			return false;
+		}
+		Estado other = (Estado) obj;
+		if(id == null){
+			if(other.id != null){
+				return false;
+			}
+		}
+		else if(!id.equals(other.id)){
+			return false;
+		}
+		if(estado != other.estado){
+			return false;
+		}
+		return true;
+	}
+}
