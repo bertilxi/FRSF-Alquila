e42a1953a740dfced6b0d40981ff203dc02e75b6
emigioria <emigioria@hotmail.com>
2016-10-22 19:25:24 -0300

    Agregada interfaz serializable a cliente

--- src/app/datos/entidades/Cliente.java
+++ src/app/datos/entidades/Cliente.java
@@ -1,5 +1,7 @@
 package app.datos.entidades;
 
+import java.io.Serializable;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -14,7 +16,9 @@ import javax.persistence.UniqueConstraint;
 
 @Entity
 @Table(name = "cliente", uniqueConstraints = @UniqueConstraint(name = "cliente_numerodocumento_idtipodocumento_uk", columnNames = { "numerodocumento", "idtipodocumento" }))
-public class Cliente {
+public class Cliente implements Serializable {
+
+	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
