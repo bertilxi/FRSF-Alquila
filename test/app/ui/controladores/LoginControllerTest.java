5daa00cb0d62bb878fb5e560a8f55a841083434a
emigioria <emigioria@hotmail.com>
2016-10-22 20:04:08 -0300

    Corregido test de muestra

--- test/app/ui/controladores/LoginControllerTest.java
+++ test/app/ui/controladores/LoginControllerTest.java
@@ -7,21 +7,18 @@ import org.junit.Test;
 import app.ui.controladores.resultado.ResultadoControlador;
 
 public class LoginControllerTest {
-	@Test
-	public void registrar() throws Exception {
-		LoginController loginController = new LoginController() {
-			@Override
-			public ResultadoControlador registrar() {
-				tfNombre.setText("Dani");
-				pfContra.setText("Dani");
-				return super.registrar();
-			}
-		};
-		assertEquals("", loginController.registrar());
-	}
 
 	@Test
 	public void ingresar() throws Exception {
+		LoginController loginController = new LoginController() {
+			@Override
+			public ResultadoControlador ingresar() {
+				tfNombre.setText("Dani");
+				pfContra.setText("Dani");
+				return super.ingresar();
+			}
+		};
+		assertEquals(new ResultadoControlador(), loginController.ingresar());
 
 	}
 
