19bdb364e1115ff520acc578af937e35e9e411f8
emigioria <emigioria@hotmail.com>
2016-10-26 15:09:59 -0300

    Eliminado PropietarioServiceMock y ViewLauncher

--- test/app/ui/controladores/AltaPropietarioControllerTest.java
+++ test/app/ui/controladores/AltaPropietarioControllerTest.java
@@ -1,13 +1,10 @@
 package app.ui.controladores;
 
-import app.ui.componentes.ViewLauncher;
 import org.junit.Test;
 
 public class AltaPropietarioControllerTest {
 	@Test
 	public void testUI() throws Exception {
-
-		ViewLauncher.launchView("/app/ui/vistas/altaPropietario.fxml");
 
 	}
 
