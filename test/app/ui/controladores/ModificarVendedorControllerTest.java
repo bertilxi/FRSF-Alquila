7d7ef7167060ac30235ebc311fd55f944ea540da
emigioria <emigioria@hotmail.com>
2016-10-26 15:09:59 -0300

    Revert "Eliminado PropietarioServiceMock y ViewLauncher"
    This reverts commit 19bdb364e1115ff520acc578af937e35e9e411f8.

--- test/app/ui/controladores/ModificarVendedorControllerTest.java
+++ test/app/ui/controladores/ModificarVendedorControllerTest.java
@@ -1,11 +1,15 @@
 package app.ui.controladores;
 
+
+import app.ui.componentes.ViewLauncher;
 import org.junit.Test;
 
 public class ModificarVendedorControllerTest {
 
-	@Test
-	public void testUI() throws Exception {
+    @Test
+    public void testUI() throws Exception {
 
-	}
+        ViewLauncher.launchView("/app/ui/vistas/modificarVendedor.fxml");
+
+    }
 }
