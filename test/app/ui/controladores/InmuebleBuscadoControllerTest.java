2115a273c78a46678abda5dc2bd78e2cc5d8dc85
emigioria <emigioria@hotmail.com>
2016-10-27 17:18:40 -0300

    Desrevertidos cambios

--- test/app/ui/controladores/InmuebleBuscadoControllerTest.java
+++ test/app/ui/controladores/InmuebleBuscadoControllerTest.java
@@ -1,14 +1,11 @@
 package app.ui.controladores;
 
-import app.ui.componentes.ViewLauncher;
 import org.junit.Test;
 
 public class InmuebleBuscadoControllerTest {
 
 	@Test
 	public void testUI() throws Exception {
-
-		ViewLauncher.launchView("/app/ui/vistas/inmuebleBuscado.fxml");
 
 	}
 
