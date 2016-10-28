678208319218d35508d70b37cb8a3bcc1e86467b
emigioria <emigioria@hotmail.com>
2016-10-28 17:08:25 -0300

    Implementada entidad archivo

--- src/app/datos/entidades/Archivo.java
+++ src/app/datos/entidades/Archivo.java
@@ -1,8 +1,15 @@
 package app.datos.entidades;
 
+import javax.persistence.Basic;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
+import javax.persistence.Lob;
 import javax.persistence.Table;
 
 @Entity
@@ -10,7 +17,12 @@ import javax.persistence.Table;
 @Inheritance(strategy = InheritanceType.JOINED)
 public abstract class Archivo {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id; //ID
 
+	@Column(name = "archivo")
+	@Lob
+	@Basic(fetch = FetchType.EAGER)
 	private byte[] archivo; //Puede ir String ruta si al final no lo ponemos en la base de datos
 }
