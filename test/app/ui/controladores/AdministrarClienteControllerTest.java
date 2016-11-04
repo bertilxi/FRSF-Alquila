41fa5def017d13f2842162e5b8f611a7e6e3507c
Fernando Berti <bertilxi@gmail.com>
2016-11-04 19:10:49 -0300

    test alta cliente

---
+++ test/app/ui/controladores/AdministrarClienteControllerTest.java
@@ -0,0 +1,17 @@
+package app.ui.controladores;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+
+@RunWith(JUnitParamsRunner.class)
+public class AdministrarClienteControllerTest {
+
+	@Test
+	@Parameters
+	public void testCrearVendedor() throws Exception {
+
+	}
+}
