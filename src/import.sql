036a2fe2d3e940b2d676df1aba86f2f32a864e90
emigioria <emigioria@hotmail.com>
2016-11-06 19:59:11 -0300

    Agregada licencia

--- src/import.sql
+++ src/import.sql
@@ -1,3 +1,21 @@
+--
+-- Copyright (C) 2016 Fernando Berti - Daniel Campodonico - Emiliano Gioria - Lucas Moretti - Esteban Rebechi - Andres Leonel Rico
+-- This file is part of Olimpo.
+--
+-- Olimpo is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- Olimpo is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with Olimpo. If not, see <http://www.gnu.org/licenses/>.
+--
+
 INSERT INTO pais VALUES (1,'Argentina');
 INSERT INTO pais VALUES (2,'Afganistán'); 
 INSERT INTO pais VALUES (3,'Akrotiri'); 
