1409b13aa2fc57d0c98c7f6a82759795190f22eb
Fernando Berti <bertilxi@gmail.com>
2016-12-09 17:56:16 -0300

    controladores modificados, agregada licencia

--- src/app/ui/controladores/AltaVentaController.java
+++ src/app/ui/controladores/AltaVentaController.java
@@ -1,17 +1,17 @@
 /**
  * Copyright (C) 2016 Fernando Berti - Daniel Campodonico - Emiliano Gioria - Lucas Moretti - Esteban Rebechi - Andres Leonel Rico
  * This file is part of Olimpo.
- *
+ * <p>
  * Olimpo is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * <p>
  * Olimpo is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- *
+ * <p>
  * You should have received a copy of the GNU General Public License
  * along with Olimpo. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -23,8 +23,11 @@ import java.util.ResourceBundle;
 
 public class AltaVentaController extends OlimpoController {
 
+    public static final String URLVista = "/app/ui/vistas/x.fxml";
+
     @Override
     protected void inicializar(URL location, ResourceBundle resources) {
+
 
     }
 }
