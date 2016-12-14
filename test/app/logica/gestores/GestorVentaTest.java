da3c3fbf68b2b1b6c97a7a37dc1c98b00bad01ba
Daniel <d.campodonico@hotmail.com>
2016-12-14 11:37:46 -0300

    Corrigiendo test que era salteado

--- test/app/logica/gestores/GestorVentaTest.java
+++ test/app/logica/gestores/GestorVentaTest.java
@@ -44,25 +44,27 @@ public class GestorVentaTest {
 	 *
 	 */
 	public void testCrearVenta(Venta venta,
-							ResultadoCrearVenta resultadoEsperado,
-							boolean resultadoCorrecto,
-							boolean medioDePagoValido,
-							Exception excepcionPersistencia,
-							Exception excepcionPDF) throws Throwable{
+			ResultadoCrearVenta resultadoEsperado,
+			boolean resultadoCorrecto,
+			boolean medioDePagoValido,
+			Exception excepcionPersistencia,
+			Exception excepcionPDF) throws Throwable {
 
 		GestorDatos gestorDatosMock = mock(GestorDatos.class);
 		GestorPDF gestorPDFMock = mock(GestorPDF.class);
 		VentaService persistidorVentaMock = mock(VentaService.class);
 		ValidadorFormato validadorMock = mock(ValidadorFormato.class);
 
-		if(excepcionPDF == null) {
+		if(excepcionPDF == null){
 			Mockito.when(gestorPDFMock.generarPDF(venta)).thenReturn(new PDF());
-		} else {
+		}
+		else{
 			Mockito.when(gestorPDFMock.generarPDF(venta)).thenThrow(excepcionPDF);
 		}
-		if(excepcionPersistencia == null) {
+		if(excepcionPersistencia == null){
 			Mockito.doNothing().when(persistidorVentaMock).guardarVenta(venta);
-		} else {
+		}
+		else{
 			Mockito.doThrow(excepcionPersistencia).when(persistidorVentaMock).guardarVenta(venta);
 		}
 		Mockito.when(validadorMock.validarMedioDePago(venta.getMedioDePago())).thenReturn(medioDePagoValido);
@@ -81,34 +83,31 @@ public class GestorVentaTest {
 		};
 
 		ResultadoCrearVenta resultadoCrearVenta;
-		try {
+		try{
 			resultadoCrearVenta = gestorVenta.crearVenta(venta);
-			assertEquals(resultadoEsperado,resultadoCrearVenta);
-		} catch (Exception e) {
-			if(excepcionPDF != null) {
-				assertEquals(excepcionPDF.getClass(),e.getClass());
+			assertEquals(resultadoEsperado.getErrores(), resultadoCrearVenta.getErrores());
+		} catch(Exception e){
+			if(excepcionPDF != null){
+				assertEquals(excepcionPDF.getClass(), e.getClass());
 			}
-			if(excepcionPersistencia != null) {
-				assertEquals(excepcionPersistencia.getClass(),e.getClass());
+			if(excepcionPersistencia != null){
+				assertEquals(excepcionPersistencia.getClass(), e.getClass());
 			}
 		}
 
 		Mockito.verify(validadorMock).validarMedioDePago(venta.getMedioDePago());
-		if(resultadoCorrecto) {
+		if(resultadoCorrecto){
 			Mockito.verify(persistidorVentaMock).guardarVenta(venta);
 			Mockito.verify(gestorPDFMock).generarPDF(venta);
 			Mockito.verify(gestorDatosMock).obtenerEstadosInmueble();
-			assertEquals(EstadoInmuebleStr.VENDIDO,venta.getInmueble().getEstadoInmueble().getEstado());
+			assertEquals(EstadoInmuebleStr.VENDIDO, venta.getInmueble().getEstadoInmueble().getEstado());
 			assertEquals(PDF.class, venta.getArchivoPDF().getClass());
-			assertEquals(Date.class,venta.getFecha().getClass());
+			assertEquals(Date.class, venta.getFecha().getClass());
 		}
 	}
 
-	private static Venta venta;
-
-	protected Object[] parametersForTestCrearVenra() {
+	protected Object[] parametersForTestCrearVenta() {
 		TipoDocumento tipoDoc = new TipoDocumento(TipoDocumentoStr.DNI);
-		venta = new Venta();
 		Propietario p = new Propietario();
 		p.setTipoDocumento(tipoDoc).setNumeroDocumento("12345678");
 		Inmueble i = new Inmueble();
@@ -120,9 +119,9 @@ public class GestorVentaTest {
 		EstadoInmueble no = new EstadoInmueble(EstadoInmuebleStr.NO_VENDIDO);
 		EstadoInmueble si = new EstadoInmueble(EstadoInmuebleStr.VENDIDO);
 		Reserva rOtro = new Reserva();
-		rOtro.setCliente(cp).setFechaInicio(new Date(System.currentTimeMillis()-1000000000)).setFechaFin(new Date(System.currentTimeMillis()+100000000));
+		rOtro.setCliente(cp).setFechaInicio(new Date(System.currentTimeMillis() - 1000000000)).setFechaFin(new Date(System.currentTimeMillis() + 100000000));
 		Reserva rMismo = new Reserva();
-		rMismo.setCliente(c).setFechaInicio(new Date(System.currentTimeMillis()-1000000000)).setFechaFin(new Date(System.currentTimeMillis()+100000000));
+		rMismo.setCliente(c).setFechaInicio(new Date(System.currentTimeMillis() - 1000000000)).setFechaFin(new Date(System.currentTimeMillis() + 100000000));
 		HashSet<Reserva> hROtro = new HashSet<>();
 		hROtro.add(rOtro);
 		HashSet<Reserva> hRMismo = new HashSet<>();
@@ -131,20 +130,20 @@ public class GestorVentaTest {
 		//Parámetros de JUnitParams
 		return new Object[] {
 				//venta,resultadoEsperado,resultadoCorrecto,medioDePagoValido,excepcionPersistencia,excepcionPDF
-				/*0*/new Object[] {venta.setCliente(c).setImporte(10.0).setInmueble(i.setEstadoInmueble(no)).setMedioDePago("contado").setPropietario(p).setVendedor(v), resultadoCorrecto, true, true, null, null }, //resultado correcto
-				/*1*/new Object[] {venta.setCliente(null).setImporte(10.0).setInmueble(i.setEstadoInmueble(no)).setMedioDePago("contado").setPropietario(p).setVendedor(v), resultadoCrearCliente_Vacío, false, true, null, null }, //la venta no tiene cliente
-				/*2*/new Object[] {venta.setCliente(c).setImporte(10.0).setInmueble(i.setEstadoInmueble(no)).setMedioDePago("contado").setPropietario(null).setVendedor(v), resultadoCrearPropietario_Vacío, false, true, null, null }, //la venta no tiene propietario
-				/*3*/new Object[] {venta.setCliente(c).setImporte(10.0).setInmueble(null).setMedioDePago("contado").setPropietario(p).setVendedor(v), resultadoCrearInmueble_Vacío, false, true, null, null }, //la venta no tiene inmueble
-				/*4*/new Object[] {venta.setCliente(c).setImporte(10.0).setInmueble(i.setEstadoInmueble(no)).setMedioDePago("contado").setPropietario(p).setVendedor(null), resultadoCrearVendedor_Vacio, false, true, null, null }, //la venta no tiene vendedor
-				/*5*/new Object[] {venta.setCliente(c).setImporte(null).setInmueble(i.setEstadoInmueble(no)).setMedioDePago("contado").setPropietario(p).setVendedor(v), resultadoCrearImporte_Vacio, false, true, null, null }, //la venta no tiene importe
-				/*6*/new Object[] {venta.setCliente(c).setImporte(10.0).setInmueble(i.setEstadoInmueble(no)).setMedioDePago(null).setPropietario(p).setVendedor(v), resultadoCrearMedio_De_Pago_Vacio, false, true, null, null }, //la venta no tiene medio de pago
-				/*7*/new Object[] {venta.setCliente(c).setImporte(10.0).setInmueble(i.setEstadoInmueble(no)).setMedioDePago("#$%&").setPropietario(p).setVendedor(v), resultadoCrearMedio_De_Pago_Incorrecto, false, false, null, null }, //formato de medio de pago incorrecto
-				/*8*/new Object[] {venta.setCliente(cp).setImporte(10.0).setInmueble(i.setEstadoInmueble(no)).setMedioDePago("contado").setPropietario(p).setVendedor(v), resultadoCrearCliente_Igual_A_Propietario, false, true, null, null }, //el cliente ya es el propietario
-				/*9*/new Object[] {venta.setCliente(c).setImporte(10.0).setInmueble(i.setEstadoInmueble(si)).setMedioDePago("contado").setPropietario(p).setVendedor(v), resultadoCrearInmueble_Ya_Vendido, false, true, null, null }, //el inmueble ya se encuentra vendido
-				/*10*/new Object[] {venta.setCliente(c).setImporte(10.0).setInmueble(i.setEstadoInmueble(no).setReservas(hROtro)).setMedioDePago("contado").setPropietario(p).setVendedor(v), resultadoCrearInmueble_Reservado_Por_Otro_Cliente, false, true, null, null }, //el inmueble ya se encuentra reservado por otro cliente
-				/*11*/new Object[] {venta.setCliente(c).setImporte(10.0).setInmueble(i.setEstadoInmueble(no).setReservas(hRMismo)).setMedioDePago("contado").setPropietario(p).setVendedor(v), resultadoCorrecto, true, true, null, null }, //resultado correcto, el inmueble está reservado pero por el mismo cliente que desea comprarlo
-				/*12*/new Object[] {venta.setCliente(c).setImporte(10.0).setInmueble(i.setEstadoInmueble(no)).setMedioDePago("contado").setPropietario(p).setVendedor(v), null, false, true, new SaveUpdateException(new Throwable()), null }, //excpecion al persistir
-				/*13*/new Object[] {venta.setCliente(c).setImporte(10.0).setInmueble(i.setEstadoInmueble(no)).setMedioDePago("contado").setPropietario(p).setVendedor(v), null, false, true, null, new GenerarPDFException(new Throwable()) }, //excpecion al generar PDF
+				/* 0 */new Object[] { new Venta().setCliente(c).setImporte(10.0).setInmueble(i.setEstadoInmueble(no)).setMedioDePago("contado").setPropietario(p).setVendedor(v), resultadoCorrecto, true, true, null, null }, //resultado correcto
+				/* 1 */new Object[] { new Venta().setCliente(null).setImporte(10.0).setInmueble(i.setEstadoInmueble(no)).setMedioDePago("contado").setPropietario(p).setVendedor(v), resultadoCrearCliente_Vacío, false, true, null, null }, //la venta no tiene cliente
+				/* 2 */new Object[] { new Venta().setCliente(c).setImporte(10.0).setInmueble(i.setEstadoInmueble(no)).setMedioDePago("contado").setPropietario(null).setVendedor(v), resultadoCrearPropietario_Vacío, false, true, null, null }, //la venta no tiene propietario
+				/* 3 */new Object[] { new Venta().setCliente(c).setImporte(10.0).setInmueble(null).setMedioDePago("contado").setPropietario(p).setVendedor(v), resultadoCrearInmueble_Vacío, false, true, null, null }, //la venta no tiene inmueble
+				/* 4 */new Object[] { new Venta().setCliente(c).setImporte(10.0).setInmueble(i.setEstadoInmueble(no)).setMedioDePago("contado").setPropietario(p).setVendedor(null), resultadoCrearVendedor_Vacio, false, true, null, null }, //la venta no tiene vendedor
+				/* 5 */new Object[] { new Venta().setCliente(c).setImporte(null).setInmueble(i.setEstadoInmueble(no)).setMedioDePago("contado").setPropietario(p).setVendedor(v), resultadoCrearImporte_Vacio, false, true, null, null }, //la venta no tiene importe
+				/* 6 */new Object[] { new Venta().setCliente(c).setImporte(10.0).setInmueble(i.setEstadoInmueble(no)).setMedioDePago(null).setPropietario(p).setVendedor(v), resultadoCrearMedio_De_Pago_Vacio, false, true, null, null }, //la venta no tiene medio de pago
+				/* 7 */new Object[] { new Venta().setCliente(c).setImporte(10.0).setInmueble(i.setEstadoInmueble(no)).setMedioDePago("#$%&").setPropietario(p).setVendedor(v), resultadoCrearMedio_De_Pago_Incorrecto, false, false, null, null }, //formato de medio de pago incorrecto
+				/* 8 */new Object[] { new Venta().setCliente(cp).setImporte(10.0).setInmueble(i.setEstadoInmueble(no)).setMedioDePago("contado").setPropietario(p).setVendedor(v), resultadoCrearCliente_Igual_A_Propietario, false, true, null, null }, //el cliente ya es el propietario
+				/* 9 */new Object[] { new Venta().setCliente(c).setImporte(10.0).setInmueble(i.setEstadoInmueble(si)).setMedioDePago("contado").setPropietario(p).setVendedor(v), resultadoCrearInmueble_Ya_Vendido, false, true, null, null }, //el inmueble ya se encuentra vendido
+				/* 10 */new Object[] { new Venta().setCliente(c).setImporte(10.0).setInmueble(i.setEstadoInmueble(no).setReservas(hROtro)).setMedioDePago("contado").setPropietario(p).setVendedor(v), resultadoCrearInmueble_Reservado_Por_Otro_Cliente, false, true, null, null }, //el inmueble ya se encuentra reservado por otro cliente
+				/* 11 */new Object[] { new Venta().setCliente(c).setImporte(10.0).setInmueble(i.setEstadoInmueble(no).setReservas(hRMismo)).setMedioDePago("contado").setPropietario(p).setVendedor(v), resultadoCorrecto, true, true, null, null }, //resultado correcto, el inmueble está reservado pero por el mismo cliente que desea comprarlo
+				/* 12 */new Object[] { new Venta().setCliente(c).setImporte(10.0).setInmueble(i.setEstadoInmueble(no)).setMedioDePago("contado").setPropietario(p).setVendedor(v), null, false, true, new SaveUpdateException(new Throwable()), null }, //excpecion al persistir
+				/* 13 */new Object[] { new Venta().setCliente(c).setImporte(10.0).setInmueble(i.setEstadoInmueble(no)).setMedioDePago("contado").setPropietario(p).setVendedor(v), null, false, true, null, new GenerarPDFException(new Throwable()) }, //excpecion al generar PDF
 
 		};
 	}
